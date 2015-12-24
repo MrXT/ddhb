ee5a321aa27ec46520c6d127dbd7c4778ddb19ae
xt <734814548@qq.com>
2015-12-24 16:35:22 +0800

    修改login成功后为用户添加角色的实力

--- ddhb/src/com/cht/ddhb/common/web/vo/SmUserVo.java
+++ ddhb/src/com/cht/ddhb/common/web/vo/SmUserVo.java
@@ -3,15 +3,57 @@ package com.cht.ddhb.common.web.vo;
 
 import java.util.List;
 
+import com.cht.ddhb.domain.CmArea;
+import com.cht.ddhb.domain.CmShop;
 import com.cht.ddhb.domain.SmRes;
+import com.cht.ddhb.domain.SmRole;
 import com.cht.ddhb.domain.SmUser;
 
 
 public class SmUserVo extends SmUser{
     private static final long serialVersionUID = -3178813338464407156L;
+    /*
+     * 登录验证码
+     */
     private String vcode;
+    /*
+     * 用户资源
+     */
     private List<SmRes> reses;
+    /*
+     * 用户角色
+     */
+    private SmRole role;
+    private CmArea area;
+    private CmShop shop;
     
+    public CmArea getArea() {
+        return area;
+    }
+
+
+    public void setArea(CmArea area) {
+        this.area = area;
+    }
+
+    
+    public CmShop getShop() {
+        return shop;
+    }
+
+    
+    public void setShop(CmShop shop) {
+        this.shop = shop;
+    }
+
+    public SmRole getRole() {
+        return role;
+    }
+    
+    public void setRole(SmRole role) {
+        this.role = role;
+    }
+
     public String getVcode() {
         return vcode;
     }
--- ddhb/src/com/cht/ddhb/module/sm/service/impl/SmUserServiceImpl.java
+++ ddhb/src/com/cht/ddhb/module/sm/service/impl/SmUserServiceImpl.java
@@ -2,12 +2,15 @@ package com.cht.ddhb.module.sm.service.impl;
 
 import java.util.List;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.cht.ddhb.common.web.vo.SmUserVo;
+import com.cht.ddhb.domain.CmArea;
+import com.cht.ddhb.domain.CmShop;
 import com.cht.ddhb.domain.SmRes;
 import com.cht.ddhb.domain.SmUser;
+import com.cht.ddhb.module.cm.dao.CmAreaDAO;
+import com.cht.ddhb.module.cm.dao.CmShopDAO;
 import com.cht.ddhb.module.sm.dao.SmResDAO;
 import com.cht.ddhb.module.sm.dao.SmUserDAO;
 import com.cht.ddhb.module.sm.service.SmUserService;
@@ -22,24 +25,38 @@ public class SmUserServiceImpl extends PaginationServiceImpl<SmUser> implements 
 	private SmUserDAO dao;
     @Autowired
     private SmResDAO  resDAO;
+    @Autowired
+    private CmAreaDAO  areaDAO;
+    @Autowired
+    private CmShopDAO  shopDAO;
 
 	@Override
 	public SmUserVo queryUserVoByUsername(String username) {
 		if(username == null) 
             throw new BusinessException("用户账号不能为空");
-		SmUser user = dao.queryUserByUsername(username);
-		SmUserVo userVo = null;
-		if(user != null){
-			userVo = new SmUserVo();
-			BeanUtils.copyProperties(user,userVo);
+		SmUserVo userVo = dao.queryUserByUsername(username);
+		if(userVo != null){
 			List<SmRes> reses = null;
 			//规则判定用户是不是超级管理员，就是看他的role_id是不是0
-			if(user.getRoleId().equals("0")){
+			if(userVo.getRoleId().equals("0")){
 				reses = resDAO.queryPagedList(new SmRes());
 			}else{
-				reses = resDAO.queryResByRoleId(user.getUserId());
+				reses = resDAO.queryResByRoleId(userVo.getRoleId());
 			}
 			userVo.setReses(reses);
+			//查询与userId对应的区域代理或者专卖店
+			CmArea area = new CmArea();
+			area.setUserId(userVo.getUserId());
+			CmShop shop = new CmShop();
+			shop.setUserId(userVo.getUserId());
+			List<CmArea> areas = areaDAO.queryPagedList(area);
+			List<CmShop> shops = shopDAO.queryPagedList(shop);
+			if(areas.size() == 1){
+			    userVo.setArea(areas.get(0));
+			}
+			if(shops.size() == 1){
+			    userVo.setShop(shops.get(0));
+			}
 		}
 		return userVo;
 	}
